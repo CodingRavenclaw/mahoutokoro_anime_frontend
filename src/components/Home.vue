9298d8cde4f82de9f0b71ee9433d793a47dcc7ee
ScienceRavenclaw <scienceravenclaw@rphg.com>
2022-03-28 13:27:41 +0200

    Created the basics of the frontend

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="mainframe">
+    <div class="row my-auto">
+      <div class="col-12">
+        <input class="form-control w-50" type="search" placeholder="Search for an anime...">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Home',
+  methods: {
+    getAnimes () {
+      console.log('Not finished yet...')
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@popperjs/core/dist/umd/popper.min'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+axios.defaults.withCredentials = true
+app.use(router)
+app.use(VueAxios, axios)
+app.provide('axios', app.config.globalProperties.axios)
+app.mount('#app')
